480dcab007a1eddc5393d117516a2cd4cbcf5069
Farooq <fmd894717@gmail.com>
2023-08-31 16:27:48 +0530

    commit 31-08-2023

--- src/main/java/cgg/springboot/restapi/springbootrestapidemo/entities/Book.java
+++ src/main/java/cgg/springboot/restapi/springbootrestapidemo/entities/Book.java
@@ -1,11 +1,20 @@
 package cgg.springboot.restapi.springbootrestapidemo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int bookId;
     private String title;
     private String author;
--- src/main/java/cgg/springboot/restapi/springbootrestapidemo/services/BooksService.java
+++ src/main/java/cgg/springboot/restapi/springbootrestapidemo/services/BooksService.java
@@ -1,50 +1,66 @@
 package cgg.springboot.restapi.springbootrestapidemo.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import cgg.springboot.restapi.springbootrestapidemo.dao.BookRepository;
 import cgg.springboot.restapi.springbootrestapidemo.entities.Book;
 
 @Component
 public class BooksService {
-    private static List<Book> books = new ArrayList<>();
 
-    static{
-        books.add(new Book(5,"Java Head First","ABC"));
-        books.add(new Book(12,"Java Cmplete First","MNO"));
-        books.add(new Book(1212,"Java Tail","JKL"));
-    }
+    @Autowired
+    private BookRepository bookRepo;
+    // private static List<Book> books = new ArrayList<>();
+
+    // static{
+    //     books.add(new Book(5,"Java Head First","ABC"));
+    //     books.add(new Book(12,"Java Cmplete First","MNO"));
+    //     books.add(new Book(1212,"Java Tail","JKL"));
+    // }
 
     //Get all books
     public List<Book> getBooks(){
-        return books;
+        List<Book> list=bookRepo.findAll();
+        return list;
     }
     //Get single book
     public Book getBookById(int id){
-       Book book= books.stream().filter(b->b.getBookId()==id).findFirst().get();
+        Book book=null;
+        try{
+                book= bookRepo.findById(id).get();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
        return book;
     }
     //Adding a book
     public Book addBook(Book book){
-        books.add(book);
-        return book;
+        Book book2=bookRepo.save(book);
+        return book2;
     }
     //Updating a book
-    public List<Book> updateBook(Book book, int id){
-        books= books.stream().map(b->{
-            if(b.getBookId()==id){
-                b.setAuthor(book.getAuthor());
-                b.setTitle(book.getTitle());
-            }
-            return b;
-        }).collect(Collectors.toList());
-        return books;
+    public void updateBook(Book book, int id){
+        // books= books.stream().map(b->{
+        //     if(b.getBookId()==id){
+        //         b.setAuthor(book.getAuthor());
+        //         b.setTitle(book.getTitle());
+        //     }
+        //     return b;
+        // }).collect(Collectors.toList());
+        Book b=bookRepo.findById(id).get();
+        b.setAuthor(book.getAuthor());
+        b.setTitle(book.getTitle());
+        Book book2=bookRepo.save(b);
+        
     }
     //Deleting a book
     public void deleteBook(int id){
-       books= books.stream().filter(b->b.getBookId()!=id).collect(Collectors.toList());
+       //books= books.stream().filter(b->b.getBookId()!=id).collect(Collectors.toList());
+       bookRepo.deleteById(id);
     }
 }
